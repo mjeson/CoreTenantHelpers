3b0d033421d0cf6b27bb528f8c8ad930af495f6f
Stuart Williams <spookdejur@hotmail.com>
2020-06-07 16:08:54 -0700

    Localization Pass 1

--- BlitzkriegSoftware.Tenant.Demo.Web/Controllers/HomeController.cs
+++ BlitzkriegSoftware.Tenant.Demo.Web/Controllers/HomeController.cs
@@ -3,6 +3,13 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.Localization;
+using System;
+
+
+// See: https://docs.microsoft.com/en-us/aspnet/core/fundamentals/localization?view=aspnetcore-3.1#make-the-apps-content-localizable
 
 namespace BlitzkriegSoftware.Tenant.Demo.Web.Controllers
 {
@@ -32,6 +39,24 @@ namespace BlitzkriegSoftware.Tenant.Demo.Web.Controllers
         public IActionResult Error()
         {
             return this.View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? this.HttpContext.TraceIdentifier });
+        }
+
+        /// <summary>
+        /// Set Langauge Post Back From Partial
+        /// </summary>
+        /// <param name="culture"></param>
+        /// <param name="returnUrl"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult SetLanguage(string culture, string returnUrl)
+        {
+            this.Response.Cookies.Append(
+                CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+                new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) }
+            );
+
+            return this.LocalRedirect(returnUrl);
         }
 
     }
